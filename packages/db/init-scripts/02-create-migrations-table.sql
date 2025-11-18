9305e64782ff7ee6114ee9175d622601096ae6ff
Vanya Slesarev <slesarevivan1401@gmail.com>
2025-11-19 02:44:37 +0300

    feat: add health checks and update configurations
    - Implement Redis, PostgreSQL, Nginx healthcheck
    - Edit postgre initialization scripts
    - Edit proxy in nginx.conf

--- packages/db/init-scripts/02-create-migrations-table.sql
+++ packages/db/init-scripts/02-create-migrations-table.sql
@@ -1,11 +1,11 @@
 \c innogram_db;
 
-CREATE TABLE IF NOT EXISTS migrations (
+CREATE TABLE IF NOT EXISTS main.migrations (
     id SERIAL PRIMARY KEY,
     timestamp BIGINT NOT NULL,
     name VARCHAR(255) NOT NULL,
     executed_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX IF NOT EXISTS idx_migrations_timestamp ON migrations (timestamp);
-CREATE INDEX IF NOT EXISTS idx_migrations_name ON migrations (name);
+--CREATE INDEX IF NOT EXISTS idx_migrations_timestamp ON migrations (timestamp);
+--CREATE INDEX IF NOT EXISTS idx_migrations_name ON migrations (name);
