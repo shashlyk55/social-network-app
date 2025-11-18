86a61e1a7a8e894f448927e9e1a3c69c885a5c0d
slesarau <ivanslesarau@innowise.com>
2025-11-18 19:16:54 +0300

    feat: implement Notifications module and edit Notification entity
    - Create Notifications module
    - Implement INotificationsService interface with method contracts
    - Develop NotificationsService
    - Create NotificationsController
    - Design Notification DTOs (CreateNotificationDto, UpdateNotificationDto, NotificationResponseDto, MarkAsReadDto)
    - Enhance Notification:
      - Create enum NotificationType for entity field

--- apps/core_microservice/src/notifications/dto/create-notification.dto.ts
+++ apps/core_microservice/src/notifications/dto/create-notification.dto.ts
@@ -1,1 +1,46 @@
-export class CreateNotificationDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsBoolean,
+  IsEnum,
+  IsObject,
+} from 'class-validator';
+import { NotificationType } from 'src/entities/notification.entity';
+
+export class CreateNotificationDto {
+  @ApiProperty({
+    example: NotificationType.LIKE,
+    description: 'Notification type',
+    enum: NotificationType,
+  })
+  @IsEnum(NotificationType)
+  @IsNotEmpty()
+  type: NotificationType;
+
+  @ApiProperty({ example: 'New like', description: 'Notification title' })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({
+    example: 'Someone liked your post',
+    description: 'Notification message',
+  })
+  @IsString()
+  @IsNotEmpty()
+  message: string;
+
+  @ApiPropertyOptional({
+    example: { postId: 1, profileId: 2 },
+    description: 'Additional data for notification',
+  })
+  @IsObject()
+  @IsOptional()
+  data?: any;
+
+  @ApiProperty({ description: 'ID of user creating the notification' })
+  @IsNotEmpty()
+  createdById: number;
+}
--- apps/core_microservice/src/notifications/notifications.module.ts
+++ apps/core_microservice/src/notifications/notifications.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { NotificationMappers } from './utils/params-mapper.util';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from 'src/entities/notification.entity';
 
 @Module({
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService, NotificationMappers],
+  exports: [NotificationsService],
+  imports: [TypeOrmModule.forFeature([Notification])],
 })
 export class NotificationsModule {}
