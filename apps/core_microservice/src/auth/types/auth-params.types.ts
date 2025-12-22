c4654e9322841f11cd028fe406c4e4412ca58025
slesarau <ivanslesarau@innowise.com>
2025-12-22 19:59:44 +0300

    feat(auth/profiles): create profile and edit auth module in auth_microservice
    - Create types for AuthService methods and responses
    - Create new empty Profiles module

--- apps/core_microservice/src/auth/types/auth-params.types.ts
+++ apps/core_microservice/src/auth/types/auth-params.types.ts
@@ -1,13 +1,14 @@
+import { Account, AccountProviderType } from 'src/entities/account.entity';
+import { User, UserRole } from 'src/entities/user.entity';
+
 export type LoginParams = {
-  login: string;
   email: string;
+  password: string;
 };
 
 export type OAuthCallbackParams = {
   authorizationCode: string;
 };
-
-export type RefreshParams = {};
 
 export type LogoutParams = {
   refreshTokenId: string;
@@ -17,4 +18,31 @@ export type ValidateTokenParams = {
   accessToken: string;
 };
 
-export type SignUpParams = {};
+export type RefreshTokenParams = {
+  refreshToken: string;
+};
+
+export type RegisterParams = {
+  email: string;
+  password: string;
+  role: UserRole;
+  provider: AccountProviderType;
+  providerId?: string;
+  createdById?: number;
+};
+
+export type AuthResult = {
+  user: User;
+  account: Account;
+  tokens: {
+    accessToken: string;
+    refreshToken: string;
+    expiresIn: number;
+  };
+};
+
+export type TokenResult = {
+  accessToken: string;
+  refreshToken: string;
+  expiresIn: number;
+};
